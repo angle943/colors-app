14e0bca98669fe7e2f905b6109865cb9b4c54504
Justin Kim <justinkim943@gmail.com>
2019-07-31 19:44:57 -0700

    added copy to clipboard functionality and animation

--- src/components/ColorBox.tsx
+++ src/components/ColorBox.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
 
 import "./ColorBox.scss";
 
@@ -9,16 +10,32 @@ interface ColorBox {
 
 const ColorBox: React.FC<ColorBox> = props => {
   const { background, name } = props;
+  const [copied, setCopied] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => {
+        setCopied(false);
+      }, 1500);
+    }
+  }, [copied]);
 
   return (
     <div className="ColorBox" style={{ background: background }}>
+      <div className={`copy-overlay ${copied && 'show'}`} style={{ background: background }} />
+      <div className={`copy-msg ${copied && 'show'}`}>
+        <h1>copied!</h1>
+        <p>{background}</p>
+      </div>
       <div className="copy-container">
         <div className="box-content">
           <span>{name}</span>
         </div>
-        <button className="copy-button">Copy</button>
+        <CopyToClipboard text={background} onCopy={() => setCopied(true)}>
+          <button className="copy-button">Copy</button>
+        </CopyToClipboard>
       </div>
-      <span className='see-more'>More</span>
+      <span className="see-more">More</span>
     </div>
   );
 };
