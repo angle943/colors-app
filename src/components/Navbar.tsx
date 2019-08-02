cfa04e54d989fccdf0a676cc3fa409f5fecffd95
Justin Kim <justinkim943@gmail.com>
2019-08-01 23:51:02 -0700

    added snackbar and styled select

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 import Slider from "rc-slider";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import "rc-slider/assets/index.css";
 
 import "./Navbar.scss";
 
 interface NavbarProps {
   changeLevel(newLevel: number): void;
+  handleChange(format: string): void;
   level: number;
 }
 
 const Navbar: React.FC<NavbarProps> = props => {
-  const {changeLevel, level} = props;
+  const { changeLevel, handleChange, level } = props;
+  const [format, setFormat] = useState<string>("hex");
+  const [showSnackbar, setShowSnackbar] = useState<boolean>(false);
+
+  const handleSelectChange = (
+    e: React.ChangeEvent<{ name?: string | undefined; value: unknown }>
+  ): void => {
+    if (!e || !e.target) return;
+    const value = e.target.value as string;
+    setFormat(value);
+    handleChange(value);
+    setShowSnackbar(true);
+  };
+
+  const closeSnackbar = (): void => {
+    setShowSnackbar(false);
+  };
 
   return (
     <header className="Navbar">
@@ -28,10 +50,34 @@ const Navbar: React.FC<NavbarProps> = props => {
             step={100}
           />
         </div>
-
       </div>
+      <div className="select-container">
+        <Select onChange={handleSelectChange} value={format}>
+          <MenuItem value="hex">HEX - #FFFFFF</MenuItem>
+          <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+          <MenuItem value="rgba">RGBA - rgb(255,255,255,1.0)</MenuItem>
+        </Select>
+      </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={showSnackbar}
+        autoHideDuration={3000}
+        message={<span id="message-id">Format Changed to {format.toUpperCase()}!</span>}
+        ContentProps={{ "aria-describedby": "message-id" }}
+        onClose={closeSnackbar}
+        action={[
+          <IconButton
+            onClick={closeSnackbar}
+            color="inherit"
+            key="close"
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+        ]}
+      />
     </header>
-  )
-}
+  );
+};
 
 export default Navbar;
--- src/components/Palette.tsx
+++ src/components/Palette.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "./Navbar";
 import ColorBox from "./ColorBox";
-import { ISeedColorWithLevels } from "../colorHelpers";
+import {ISeedColorWithLevels} from "../colorHelpers";
 
 import "./Palette.scss";
 
@@ -11,12 +11,19 @@ interface PaletteProps {
 
 const Palette: React.FC<PaletteProps> = props => {
   const { palette } = props;
+  const [format, setFormat] = useState<"hex" | "rgb" | "rgba">("hex");
   const [level, setLevel] = useState<number>(500);
 
   const renderColorBoxes = (): React.ReactNode => {
-    return palette.colors[level].map(color => (
-      <ColorBox key={color.hex} background={color.hex} name={color.name} />
-    ));
+    return palette.colors[level].map((color) => {
+      console.log("COLOR", color);
+      return (
+      <ColorBox key={color.hex} background={color[format]} name={color.name} />
+    )});
+  };
+
+  const changeFormat = (format: "hex" | "rgb" | "rgba") => {
+    setFormat(format);
   };
 
   const changeLevel = (newLevel: number): void => {
@@ -25,7 +32,11 @@ const Palette: React.FC<PaletteProps> = props => {
 
   return (
     <div className="Palette">
-      <Navbar changeLevel={changeLevel} level={level} />
+      <Navbar
+        changeLevel={changeLevel}
+        level={level}
+        handleChange={changeFormat}
+      />
       <div className="Palette-colors">{renderColorBoxes()}</div>
     </div>
   );
